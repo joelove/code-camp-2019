ea3a557b46900b7b927e8dad825c0fb4abdccc74
Mark Voit <mark.voit@futurice.com>
2019-09-21 16:13:43 +0300

    some refactorings

--- play.py
+++ play.py
@@ -16,13 +16,42 @@ BALL_COLOR_UPPER1 = (5, 255, 255)
 BALL_COLOR_LOWER2 = (175, 155, 100)
 BALL_COLOR_UPPER2 = (180, 255, 255)
 
+BALL_CIRCLE_COLOR = (0, 255, 255)
+
 GOAL_COLOR_LOWER = (25, 0, 180)
 GOAL_COLOR_UPPER = (45, 100, 255)
+
+GOAL_FILL_COLOR = (255, 0, 255)
+
+PLAYER_NAME_COLOR = (50, 255, 100)
+
+SCORE_TEXT_COLOR = (255, 255, 255)
+SCORE_RECT_COLOR = (0, 0, 0)
 
 BUFFER_SIZE = 64
 
 MAX_SCORE = 5
 GOAL_MIN_AREA = 700
+
+def init_players():
+    players = []
+    while not len(players) == 2:
+        frame = read_frame()
+        draw_start_text(frame)
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(1)
+        players = find_players()
+
+    print(f'found {len(players)} players')
+    return players
+
+
+def init_dummy_players():
+    print('using dummy players')
+    return [
+        { "name": "Foo" },
+        { "name": "Bar" }
+    ]
 
 
 def create_player(face):
@@ -54,10 +83,10 @@ def generate_ball_mask(image):
     ball_mask = cv2.erode(ball_mask, None, iterations=2)
     ball_mask = cv2.dilate(ball_mask, None, iterations=2)
 
-    return ball_mask
+    return ball_mask.copy()
 
 
-def generate_ball_contours(mask):
+def detect_ball_contours(mask):
     ball_contours = cv2.findContours(mask, cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
     ball_contours = imutils.grab_contours(ball_contours)
@@ -80,9 +109,11 @@ def generate_goal_mask(image):
     return goal_mask.copy()
 
 
-def generate_goal_contours(mask):
+def detect_goal_contours(image):
+    mask = generate_goal_mask(image)
     goal_contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     goal_contours = [contour for contour in goal_contours if cv2.contourArea(contour) > GOAL_MIN_AREA]
+    print(f'found {len(goal_contours)} goals')
 
     return goal_contours
 
@@ -103,15 +134,15 @@ def draw_ball_circle(ball_contours):
         ball_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
         if radius > 5:
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-            cv2.circle(frame, ball_center, 5, (0, 0, 255), -1)
+            cv2.circle(frame, (int(x), int(y)), int(radius), BALL_CIRCLE_COLOR, 2)
+            cv2.circle(frame, ball_center, 5, BALL_CIRCLE_COLOR, -1)
 
     return ball_center
 
 
 def draw_goals(frame, goal_contours):
     if len(goal_contours) > 0:
-        cv2.drawContours(frame, goal_contours, -1, (255, 0, 255), -1)
+        cv2.drawContours(frame, goal_contours, -1, GOAL_FILL_COLOR, -1)
 
 
 def draw_ball_tracking_points(frame, ball_tracking_points):
@@ -127,17 +158,17 @@ def draw_scores(frame, scores, players):
     if player_one_won(scores) or player_two_won(scores):
         player_index = 0 if player_one_won(scores) else 1
         player_text = f'{players[player_index]["name"]} won!'
-        cv2.putText(frame, player_text, (130, 150), 0, 2, (255, 255, 255), 4)
+        cv2.putText(frame, player_text, (130, 150), 0, 2, SCORE_TEXT_COLOR, 4)
     else:
-        cv2.rectangle(frame, (180,0), (420, 60), (0, 0, 0), -1)
-        cv2.putText(frame, f'{int(scores[0])} - {int(scores[1])}', (200, 50), 0, 2, (255, 255, 255), 4)
+        cv2.rectangle(frame, (180,0), (420, 60), SCORE_RECT_COLOR, -1)
+        cv2.putText(frame, f'{int(scores[0])} - {int(scores[1])}', (200, 50), 0, 2, SCORE_TEXT_COLOR, 4)
 
 
 def draw_player_names(frame, players):
     if len(players) > 0:
-        cv2.putText(frame, players[0]["name"], (10, 30), 0, 1, (0, 255, 0), 4)
+        cv2.putText(frame, players[0]["name"], (10, 30), 0, 1, PLAYER_NAME_COLOR, 4)
     if len(players) > 1:
-        cv2.putText(frame, players[1]["name"], (500, 30), 0, 1, (0, 255, 0), 4)
+        cv2.putText(frame, players[1]["name"], (500, 30), 0, 1, PLAYER_NAME_COLOR, 4)
 
 
 def draw_start_text(frame):
@@ -163,27 +194,14 @@ blank_frame = np.zeros(frame.shape[0:2])
 ball_tracking_points = deque(maxlen=BUFFER_SIZE)
 scores = np.zeros(2)
 
-is_in_goal = 0
+players = init_dummy_players()
 
-# detect goals
-goal_mask = generate_goal_mask(hsv)
-goal_contours = generate_goal_contours(goal_mask)
+goal_contours = detect_goal_contours(hsv)
 goal_0_image = get_goal_image(goal_contours, 0)
 goal_1_image = get_goal_image(goal_contours, 1)
 
-print(f'found {len(goal_contours)} goals')
-
+is_in_goal = 0
 frame_count = 1
-players = []
-
-while not len(players) == 2:
-    frame = read_frame()
-    draw_start_text(frame)
-    cv2.imshow("Frame", frame)
-    cv2.waitKey(1)
-    players = find_players()
-
-print(f'found {len(players)} players')
 
 while True:
     frame = read_frame()
@@ -194,7 +212,7 @@ while True:
     draw_scores(frame, scores, players)
 
     ball_mask = generate_ball_mask(hsv)
-    ball_contours = generate_ball_contours(ball_mask.copy())
+    ball_contours = detect_ball_contours(ball_mask)
     ball_image = cv2.drawContours(blank_frame.copy(), ball_contours, 0, 1, -1)
 
     ball_center = draw_ball_circle(ball_contours)
@@ -207,7 +225,7 @@ while True:
     if goal_0_collision and is_in_goal == 0:
         scores[0] += 1
         is_in_goal = 1
-        print(f'detected goal for palyer {players[0]["name"]}')
+        print(f'detected goal for player {players[0]["name"]}')
 
     if goal_1_collision and is_in_goal == 0:
         scores[1] += 1
@@ -219,11 +237,8 @@ while True:
 
     cv2.imshow("Frame", frame)
 
-    if (frame_count % 1000 == 0):
-        # detect goals
-        goal_mask = generate_goal_mask(hsv)
-        goal_contours = generate_goal_contours(goal_mask)
-        print(f'found {len(goal_contours)} goals')
+    if (frame_count % 500 == 0):
+        goal_contours = detect_goal_contours(hsv)
         goal_0_image = get_goal_image(goal_contours, 0)
         goal_1_image = get_goal_image(goal_contours, 1)
         frame_count = 1
